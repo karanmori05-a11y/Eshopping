afe79dd621247bfd1639458d516c0eebab26f48a
Karan <karanmori05@gmail.com>
2026-01-08 16:33:53 +0530

    project upload

---
+++ images/products/image.php
@@ -0,0 +1,10 @@
+$products = [
+    ["name"=>"Blue Denim Jeans 1","image"=>"images/jeans1.jpg","price"=>1200,"stock"=>10],
+    ["name"=>"Blue Denim Jeans 2","image"=>"images/jeans2.jpg","price"=>1250,"stock"=>8],
+    ["name"=>"Blue Denim Jeans 3","image"=>"images/jeans3.jpg","price"=>1300,"stock"=>6],
+    ["name"=>"Black Fit Jeans 1","image"=>"images/jeans4.jpg","price"=>1400,"stock"=>12],
+    ["name"=>"Black Fit Jeans 2","image"=>"images/jeans5.jpg","price"=>1450,"stock"=>9],
+    ["name"=>"Black Fit Jeans 3","image"=>"images/jeans6.jpg","price"=>1500,"stock"=>7],
+    ["name"=>"Grey Casual Jeans 1","image"=>"images/jeans7.jpg","price"=>1350,"stock"=>11],
+    ["name"=>"Grey Casual Jeans 2","image"=>"images/jeans8.jpg","price"=>1380,"stock"=>13],
+];
